8fde0901b6e9621de23be2a5a85fc3d014640906
evar-7 <L.ev.var52@gmail.com>
2025-03-30 21:34:23 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/sistemainventario/SistemaInventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemainventario;
+
+/**
+ *
+ * @author PC
+ */
+public class SistemaInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
