69b4aedbfa7753a52d18479fc5b9147dcae8c0a1
jmnpcd <picadojimena906@gmail.com>
2025-03-30 21:43:39 -0600

    Update SistemaInventario.java

--- src/main/java/com/mycompany/sistemainventario/SistemaInventario.java
+++ src/main/java/com/mycompany/sistemainventario/SistemaInventario.java
@@ -13,5 +13,6 @@ public class SistemaInventario {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Prueba");
+        System.out.println("Prueba2");
     }
 }
