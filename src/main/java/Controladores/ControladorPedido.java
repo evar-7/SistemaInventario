17626f74ec16c0b3758b15fd5d33f2eb9163243a
evar-7 <L.ev.var52@gmail.com>
2025-04-12 13:11:29 -0600

    Se agregó lógica de ControladorPedido

--- src/main/java/Controladores/ControladorPedido.java
+++ src/main/java/Controladores/ControladorPedido.java
@@ -4,10 +4,27 @@
  */
 package Controladores;
 
+import Modelo.Pedido;
+import Modelo.PedidoDAO;
+
 /**
  *
  * @Erick
+ *  private int id_pedido;
+    private int id_usuario;
+    private java.util.Date fecha_pedido;
+    private String estado;
+    private double total;
  */
 public class ControladorPedido {
-    
+    private PedidoDAO pedidoDAO = new PedidoDAO();
+
+    public boolean crearPedido(int id_usuario) {
+        Pedido nuevoPedido = new Pedido(0, id_usuario, new java.util.Date(), "pendiente");
+        return pedidoDAO.crearPedido(nuevoPedido);
+    }
+
+    public boolean actualizarEstado(int idPedido, String nuevoEstado) {
+        return pedidoDAO.actualizarEstado(idPedido, nuevoEstado);
+    }
 }
