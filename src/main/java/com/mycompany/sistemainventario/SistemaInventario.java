0e987a98fd53139d4a96860719525a2f928300d2
evar-7 <L.ev.var52@gmail.com>
2025-04-21 00:34:26 -0600

    Se modificó el main para inicializar VistaProductos

--- src/main/java/com/mycompany/sistemainventario/SistemaInventario.java
+++ src/main/java/com/mycompany/sistemainventario/SistemaInventario.java
@@ -1,25 +1,18 @@
 
 package com.mycompany.sistemainventario;
-
-import Modelo.Pedido;
-import Modelo.Usuario;
-import java.text.SimpleDateFormat;
+import Controladores.ControladorLogin;
+import Controladores.ControladorProductos;
+import Vista.Login;
+import Vista.VistaProductos;
 
 
 public class SistemaInventario {
 
     public static void main(String[] args) {
-       /* SimpleDateFormat formato = new SimpleDateFormat();
-        Pedido p1 = new Pedido();
-        Usuario u1= new Usuario();
-        
-        p1.setEstado("estado");
-        p1.setFecha_pedido(formato.parse("12-12-2025"));
-        p1.setId_usuario(1);
-        p1.setId_usuario(1);
-        
-        
-        p1.crearPedido(producto);*/
-        
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            VistaProductos vista = new VistaProductos();
+            new ControladorProductos(vista);
+            vista.setVisible(true);
+        });
     }
 }
